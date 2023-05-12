d90c3fc7de181c19873752bf190c69012df0c4f2
hui.yang <hui.yang@bangdao-tech.com>
2023-05-12 11:51:08 +0800

    测试

--- src/App.vue
+++ src/App.vue
@@ -5,17 +5,6 @@ onLaunch(() => {
 });
 onShow(() => {
   console.log('App Show');
-  console.log(111111);
-  console.log(111111);
-  console.log(111111);
-  console.log(111111);
-  console.log(111111);
-  console.log(111111);
-  console.log(111111);
-  console.log(111111);
-  console.log(111111);
-  console.log(111111);
-  console.log(111111);
 });
 onHide(() => {
   console.log('App Hide');
